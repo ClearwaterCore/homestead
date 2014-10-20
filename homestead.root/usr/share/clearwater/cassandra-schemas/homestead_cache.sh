1cbc52d7ea3b3025b05a15f98155d23cf547d81f
root <root@ubuntu.(none)>
2014-10-20 05:21:23 -0700

    Pick up V5 change

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
+
+netstat -na | grep -q ":7199[^0-9]"
+while [ $? -ne 0 ]; do
+    sleep 1
+    printf "."
+    netstat -na | grep -q ":7199[^0-9]"
+done
+netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
+while [ $? -ne 0 ]; do
+    sleep 1
+    printf "+"
+    netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
+done
+
 if [[ ! -e /var/lib/cassandra/data/homestead_cache ]];
 then
     echo "CREATE KEYSPACE homestead_cache WITH strategy_class = 'SimpleStrategy' AND strategy_options:replication_factor = 2;
@@ -6,6 +20,12 @@ USE homestead_cache;
 CREATE TABLE impi (private_id text PRIMARY KEY, digest_ha1 text, digest_realm text, digest_qop text, known_preferred boolean) WITH read_repair_chance = 1.0;
 CREATE TABLE impu (public_id text PRIMARY KEY, ims_subscription_xml text, is_registered boolean) WITH read_repair_chance = 1.0;" | cqlsh -2
 fi
+
+echo "USE homestead_cache;
+ALTER TABLE impu ADD primary_ccf text;
+ALTER TABLE impu ADD secondary_ccf text;
+ALTER TABLE impu ADD primary_ecf text;
+ALTER TABLE impu ADD secondary_ecf text;" | cqlsh -2
 
 if [[ ! -e /var/lib/cassandra/data/homestead_cache/impi_mapping ]];
 then
