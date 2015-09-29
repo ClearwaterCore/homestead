8c417682336053360059ea1e8efbeb584cb22605
Doug Theobald <doug.theobald@metaswitch.com>
2015-09-29 11:05:22 -0700

    Factor out common code [CC-only] (CC_cassredun-82)

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
@@ -1,46 +1,22 @@
-#! /bin/bash
+#!/bin/bash
 
+keyspace=$(basename $0|sed -e 's#^\(.*\)[.]sh$#\1#')
 . /etc/clearwater/config
-if [ ! -z $signaling_namespace ]
-then
-  if [ $EUID -ne 0 ]
-  then
-    echo "When using multiple networks, schema creation must be run as root"
-    exit 2
-  fi
-  namespace_prefix="ip netns exec $signaling_namespace"
+if [ ! -z $signaling_namespace ]; then
+    if [ $EUID -ne 0 ]; then
+        echo "When using multiple networks, schema creation must be run as root"
+        exit 2
+    fi
+    namespace_prefix="ip netns exec $signaling_namespace"
 fi
+
+$(dirname $0)/../bin/wait4cassandra ${keyspace}
+if [ $? -ne 0 ]; then
+    exit 1
+ fi
 
 if [[ ! -e /var/lib/cassandra/data/homestead_cache ]];
 then
-  header="Waiting for Cassandra"
-  let "cnt=0"
-  $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-  while [ $? -ne 0 ]; do
-    sleep 1
-    printf "${header}."
-    header=""
-    let "cnt=$cnt + 1"
-    if [ $cnt -gt 120 ]; then
-      printf "*** ERROR: Cassandra did not come online!\n"
-      exit 1
-    fi
-    $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-  done
-  let "cnt=0"
-  $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
-  while [ $? -ne 0 ]; do
-    sleep 1
-    printf "${header}+"
-    header=""
-    let "cnt=$cnt + 1"
-    if [ $cnt -gt 120 ]; then
-      printf "*** ERROR: Cassandra did not come online!\n"
-      exit 1
-    fi
-    $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
-  done
-
   echo "CREATE KEYSPACE homestead_cache WITH REPLICATION =  {'class': 'SimpleStrategy', 'replication_factor': 2};
         USE homestead_cache;
         CREATE TABLE impi (private_id text PRIMARY KEY, digest_ha1 text, digest_realm text, digest_qop text, known_preferred boolean) WITH COMPACT STORAGE AND read_repair_chance = 1.0;
