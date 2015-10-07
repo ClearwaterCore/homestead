275d6ec50826378f72714b516fe422b07b2fe466
Doug Theobald <doug.theobald@metaswitch.com>
2015-10-07 08:41:25 -0700

    Merge cw-cass issue #54 fix

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-
-keyspace=$(basename $0|sed -e 's#^\(.*\)[.]sh$#\1#')
-. /etc/clearwater/config
-if [ ! -z $signaling_namespace ]; then
-    if [ $EUID -ne 0 ]; then
-        echo "When using multiple networks, schema creation must be run as root"
-        exit 2
-    fi
-    namespace_prefix="ip netns exec $signaling_namespace"
-fi
-
-$(dirname $0)/../bin/wait4cassandra ${keyspace}
-if [ $? -ne 0 ]; then
-    exit 1
- fi
+#! /bin/bash
 
 if [[ ! -e /var/lib/cassandra/data/homestead_cache ]];
 then
+  # Wait for the cassandra cluster to come online
+  count=0
+  /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
+
+  while [ $? -ne 0 ]; do
+    ((count++))
+    if [ $count -gt 120 ]; then
+      echo "Cassandra isn't responsive, unable to add schemas"
+      exit 1
+    fi
+
+    sleep 1
+    /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
+  done
+
   echo "CREATE KEYSPACE homestead_cache WITH REPLICATION =  {'class': 'SimpleStrategy', 'replication_factor': 2};
         USE homestead_cache;
         CREATE TABLE impi (private_id text PRIMARY KEY, digest_ha1 text, digest_realm text, digest_qop text, known_preferred boolean) WITH COMPACT STORAGE AND read_repair_chance = 1.0;
-        CREATE TABLE impu (public_id text PRIMARY KEY, ims_subscription_xml text, is_registered boolean) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | $namespace_prefix cqlsh
+        CREATE TABLE impu (public_id text PRIMARY KEY, ims_subscription_xml text, is_registered boolean) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
 fi
 
 echo "USE homestead_cache; DESC TABLE impu" | cqlsh | grep primary_ccf > /dev/null
@@ -29,11 +29,11 @@ if [ $? != 0 ]; then
         ALTER TABLE impu ADD primary_ccf text;
         ALTER TABLE impu ADD secondary_ccf text;
         ALTER TABLE impu ADD primary_ecf text;
-        ALTER TABLE impu ADD secondary_ecf text;" | $namespace_prefix cqlsh
+        ALTER TABLE impu ADD secondary_ecf text;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
 fi
 
 if [[ ! -e /var/lib/cassandra/data/homestead_cache/impi_mapping ]];
 then
   echo "USE homestead_cache;
-        CREATE TABLE impi_mapping (private_id text PRIMARY KEY, unused text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | $namespace_prefix cqlsh
+        CREATE TABLE impi_mapping (private_id text PRIMARY KEY, unused text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
 fi
