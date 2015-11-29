d3b584ba98b8b72596105a95fc22d086d2b1b1e9
Rob Day <Robert.Day@metaswitch.com>
2015-11-29 16:46:32 +0000

    Track a 'database migration number' so we don't re-run cqlsh on every clearwater-infrastructure restart

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_cache.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 . /etc/clearwater/config
 
+MIGRATION_NUM=1
+MIGRATION_FILE=/var/lib/homestead/cassandra-migration
+
+if [[ -e $MIGRATION_FILE ]]
+then
+  CURRENT_MIGRATION_VALUE=$(cat $MIGRATION_FILE)
+else
+  CURRENT_MIGRATION_VALUE=0
+fi
+
 if [[ ! -e /var/lib/cassandra/data/homestead_cache ]];
 then
   # Wait for the cassandra cluster to come online
@@ -14,6 +24,7 @@ then
       exit 1
     fi
 
+    echo "Cassandra not up yet, polling again"
     sleep 1
     /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
   done
@@ -22,29 +33,37 @@ then
         USE homestead_cache;
         CREATE TABLE impi (private_id text PRIMARY KEY, digest_ha1 text, digest_realm text, digest_qop text, known_preferred boolean) WITH COMPACT STORAGE AND read_repair_chance = 1.0;
         CREATE TABLE impu (public_id text PRIMARY KEY, ims_subscription_xml text, is_registered boolean) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
+
+  # Make sure we re-run any changes if the database has been deleted
+  CURRENT_MIGRATION_VALUE=0
 fi
 
-echo "USE homestead_cache; DESC TABLE impu" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh | grep primary_ccf > /dev/null
-if [ $? != 0 ]; then
-  echo "USE homestead_cache;
-        ALTER TABLE impu ADD primary_ccf text;
-        ALTER TABLE impu ADD secondary_ccf text;
-        ALTER TABLE impu ADD primary_ecf text;
-        ALTER TABLE impu ADD secondary_ecf text;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
-fi
-
-if [[ ! -e /var/lib/cassandra/data/homestead_cache/impi_mapping ]];
+if [[ -e /var/lib/cassandra/data/homestead_cache/impi_mapping ]];
 then
   echo "USE homestead_cache;
-        CREATE TABLE impi_mapping (private_id text PRIMARY KEY, unused text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
+  CREATE TABLE impi_mapping (private_id text PRIMARY KEY, unused text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
 fi
 
-if [ -z "$speculative_retry_value" ]
+if [[ $CURRENT_MIGRATION_VALUE != $MIGRATION_NUM ]]
 then
-  speculative_retry_value="50ms"
+  echo "USE homestead_cache; DESC TABLE impu" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh | grep primary_ccf > /dev/null
+  if [ $? != 0 ]; then
+    echo "USE homestead_cache;
+          ALTER TABLE impu ADD primary_ccf text;
+          ALTER TABLE impu ADD secondary_ccf text;
+          ALTER TABLE impu ADD primary_ecf text;
+          ALTER TABLE impu ADD secondary_ecf text;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
+  fi
+
+  if [ -z "$speculative_retry_value" ]
+  then
+    speculative_retry_value="50ms"
+  fi
+
+  echo "USE homestead_cache;
+        ALTER TABLE impu WITH speculative_retry = '$speculative_retry_value';
+        ALTER TABLE impi_mapping WITH speculative_retry = '$speculative_retry_value';
+        ALTER TABLE impi WITH speculative_retry = '$speculative_retry_value';" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
 fi
 
-echo "USE homestead_cache;
-      ALTER TABLE impu WITH speculative_retry = '$speculative_retry_value';
-      ALTER TABLE impi_mapping WITH speculative_retry = '$speculative_retry_value';
-      ALTER TABLE impi WITH speculative_retry = '$speculative_retry_value';" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
+echo $MIGRATION_NUM > $MIGRATION_FILE
